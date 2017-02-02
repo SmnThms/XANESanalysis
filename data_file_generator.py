672ed09c71b28408ccfc994e09d13af5b31a8659
SmnThms <simon.thomas8@gmail.com>
2017-02-02 12:17:07 +0100

    Add files via upload

--- data_file_generator.py
+++ data_file_generator.py
@@ -11,53 +11,59 @@ from matplotlib import pyplot as plt
 import xlrd
 
 source_folder = '.\\Raw_data\\'
+destination_folder = '.\\Data\\'
 type_of_set = 'QE'
 
-# For each scan : (start time, end time,)
-C = {'Al':((1,13600),(36405,40905))}
-C.update({'Al2':((36405,40905))})
-C.update({'AlLi':((26220,35556))})
-C.update({'Alrp':((6925,15730),(18514,22819))})
-C.update({'AlLirp':((33600,41300))})
+# For each set of scans : (start time of first scan, start time of last scan,)
+C = {'Al':(1,6303)}#13600)}
+C.update({'AlLi':(13647,18050)})#(26220,35556)})
+C.update({'Al2':(18948,20960)})#(36405,40905)})
+C.update({'Alrp':(3605,7900)})#(6925,15730)})
+C.update({'Alrp2':(9636,11500)})#(18514,22819)})
+C.update({'AlLirp':(17492,21000)})#(33600,41300)})
 
 if type_of_set is 'QE':
-    for filename, names_of_sets in zip(['Monitor_20120926_20h32','monitor2809_07h48'],[['Al','Al2','AlLi']['Alrp','AlLirp']]):
+    for filename, names_of_sets in zip(['Monitor_20120926_20h32','monitor2809_07h48'],[['Al','AlLi','Al2'],['Alrp','Alrp2','AlLirp']]):
         wb = xlrd.open_workbook(source_folder+filename+'.xls')
         sheet = wb.sheet_by_index(0)
         data = np.zeros((sheet.nrows,sheet.ncols))
-        for row_nb in range(sheet.nrows-2): 
+        for row_nb in range(sheet.nrows-2): # Copy of the xls file in an array
             data[row_nb,:] = np.array([sheet.row_values(row_nb+2)])
-        l1 = [sheet.row_values(0)]
+        l1 = [str(title) for title in sheet.row_values(0)] # Titles of the columns
         for name_of_set in names_of_sets:
-            row_nb = C[name_of_set]
-            for scan_nb in range(len(row_nb)):
+            scan_nb = 0            
+            row_nb = C[name_of_set][0]
+            while row_nb < C[name_of_set][1]:
+                exported_data = []
+                while not data[row_nb,l1.index('LASER_ON')]:
+                    row_nb += 1
+                while data[row_nb,l1.index('LASER_ON')]:
+                    exported_data.append([data[row_nb,l1.index(column)] for column in ['time','I_KEITH2','P_ES','TEMP']])
+                    row_nb += 1
                 parameters_names = ['Date','Hour','Laser_intensity','Photocurrent_unit','','Time\tPhotocurrent(unit)\tPressure(unit)\tTemperature(unit)']
                 parameters_values = ['','']
                 header = '\n'.join(['\t'.join((i,str(j))) for i,j in zip(parameters_names,parameters_values)])
-                exported_data = np.zeros(row_nb[1]-row_nb[0])
-                exported_data[:,0] = data[:,l1.index('time')]
-                exported_data[:,1] = data[:,l1.index('I_KEITH2')]
-                exported_data[:,2] = data[:,l1.index('P_ES')]
-                exported_data[:,3] = data[:,l1.index('TEMP')]
-                destination = '.\\Data\\' + name_of_set + '_' + type_of_set + '_' + str(scan_nb) + '.txt'
-                np.savetxt(destination,exported_data,header=header)
+                destination = destination_folder + name_of_set + '_' + type_of_set + '_' + str(scan_nb) + '.txt'
+                np.savetxt(destination,np.array(exported_data),header=header)
+                scan_nb += 1
+                print name_of_set, row_nb
     
 
 elif type_of_set is 'XANES':
-    for name_of_set in ['Al','Al2','Alrp','AlLi','AlLirp']:
+    for name_of_set in ['Al','Al2','Alrp','Alrp2','AlLi','AlLirp']:
         for scan_nb,source_file in enumerate(os.listdir(source_folder+name_of_set)):
             source_directory = source_folder + name_of_set + '\\' + source_file
             l1 = open(source_directory).readline().split('\t')
             data = np.loadtxt(source_directory)
-            parameters_names = ['Date','Hour','','Energy(eV)\tPhotocurrent(unit)\tTopUp\tMonochromator']
+            parameters_names = ['Date','Hour','','Energy(eV)\tPhotocurrent(unit)\tTopUp\tI0']
             parameters_values = [source_file.split('_')[1],source_file.split('_')[2],'','']
             header = '\n'.join(['\t'.join((i,str(j))) for i,j in zip(parameters_names,parameters_values)])
             exported_data = np.zeros((len(data),4))
             exported_data[:,0] = data[:,l1.index('Energy')]
             exported_data[:,1] = data[:,l1.index('I1_KEITHLEY2')]
             exported_data[:,2] = data[:,l1.index('I_SLS')] #TopUp
-            exported_data[:,3] = data[:,l1.index('I0_KEITHLEY1')] #Monochromator, only valid before the repolishing
-            destination = '.\\Data\\' + name_of_set + '_' + type_of_set + '_' + str(scan_nb) + '.txt'
+            exported_data[:,3] = data[:,l1.index('I0_KEITHLEY1')] #I0 for normalization, only valid before the repolishing
+            destination = destination_folder + name_of_set + '_' + type_of_set + '_' + str(scan_nb) + '.txt'
             np.savetxt(destination,exported_data,header=header)
 
 def plot(column):
--- figure_generator.py
+++ figure_generator.py
@@ -5,55 +5,71 @@ Created on Tue Jan 24 22:35:38 2017
 @author: Simon
 """
 
+from __future__ import division
 import numpy as np
 from matplotlib import pyplot as plt
 
 class Scan:
     def __init__(self,name_of_scan,type_of_scan,number_of_scan):
-        file_address = name_of_scan + '_' + type_of_scan + '_' + number_of_scan + '.dat'
+        file_address = '.\\Data\\' + name_of_scan + '_' + type_of_scan + '_' + str(number_of_scan) + '.txt'
         data_file = open(file_address,'rb')
         self.initial_time = data_file.readline().split('\t')[1]       
         self.final_time = data_file.readline().split('\t')[1]
         data = np.loadtxt(file_address)
         
         if type_of_scan is 'QE':
-            self.time = 
-            self.QE = 
-            self.pressure =
-            self.laser_power = 
+            self.time = data[:,0] - data[0,0]
+            self.QE = data[:,1]
+            self.pressure = data[:,2]
+            self.laser_power = data[:,3]
 
         elif type_of_scan is 'XANES':
             self.energy = data[:,0]
-            self.photocurrent = data[:,1].normalize()
+            self.topup = data[:,2]
+            self.I0 = data[:,3]
+            self.photocurrent = data[:,1]
+            self.normalize()
             
     def fit(self):
         return 0
         
     def normalize(self):
-        self.normalized_photocurrent = 
+        self.photocurrent *= 1/self.topup
+        N = 10
+        self.photocurrent -= np.polyval(np.polyfit(self.energy[:N],self.photocurrent[:N],1),self.energy)
         
-class Set_of_scans:
+        
+class Set_of:
     def __init__(self,name_of_scan,type_of_scan):
+        nb = {'Al':5,'Al2':5}
+        self.scans = [Scan(name_of_scan,type_of_scan,number_of_scan) for number_of_scan in range(nb[name_of_scan])]
         
-    def superimpose(self):
+    def scaling(self):
+        for scan in self.scans:
+            reference = 0
+            coefs = np.arange(10000)*2/10000
+            diff = [np.sum(abs(coef*scan.photocurrent - self.scans[reference].photocurrent)) for coef in coefs]
+            scan.photocurrent *= coefs[np.argmin(diff)]
         
-        
-        
-def plot_XANES(set_of_scans, format_of_figure):
-    figure = plt.plot()
-    if format_of_figure is 'big':
-        
-    elif format_of_figure is 'small':
-        
-    print 'Name, type, parameters of the scan'
-    return figure
-    
-#def plot_QE():
-#    
-#def plot_correlation_QE_XANES_P():
-#    
-#def plot_correlation_CS_I():
+#def plot_XANES(set_of_scans, format_of_figure):
+#    figure = plt.plot()
+#    if format_of_figure is 'big':
 #        
-#def plot_all_sets_of_scans():
-#
-#def plot_fit():
+#    elif format_of_figure is 'small':
+#        
+#    print 'Name, type, parameters of the scan'
+#    return figure
+    
+##def plot_QE():
+##    
+##def plot_correlation_QE_XANES_P():
+##    
+##def plot_correlation_CS_I():
+##        
+##def plot_all_sets_of_scans():
+##
+##def plot_fit():
+
+s = Scan('AlLirp','XANES',0)
+#plt.plot(s.time,s.QE)
+plt.plot(s.energy,s.photocurrent)
