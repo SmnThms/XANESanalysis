224cf3e2c5794b9a7ca4b2896bdecb39a4d4daf1
SmnThms <simon.thomas8@gmail.com>
2017-05-29 12:13:05 +0200

    Add files via upload

--- figure_generator.py
+++ figure_generator.py
@@ -8,6 +8,8 @@ Created on Tue Jan 24 22:35:38 2017
 from __future__ import division
 import numpy as np
 from matplotlib import pyplot as plt
+from matplotlib import colors as col
+
 
 class Scan:
     def __init__(self,name_of_scan,type_of_scan,number_of_scan):
@@ -41,10 +43,12 @@ class Scan:
         
 class Set_of:
     def __init__(self,name_of_scan,type_of_scan):
-        nb = {'Al':5,'Alrp':5}
+        nb = {'Al':6,'Alrp':9}
         self.name = name_of_scan
-        self.scans = [Scan(name_of_scan,type_of_scan,number_of_scan) for number_of_scan in range(nb[name_of_scan])]
-        self.scaling()
+        self.length = nb[name_of_scan]
+        self.scans = [Scan(name_of_scan,type_of_scan,number_of_scan) for number_of_scan in range(self.length)]
+        if type_of_scan is 'XANES':
+            self.scaling()
         
     def scaling(self):
         for scan in self.scans:
@@ -52,20 +56,44 @@ class Set_of:
             diff = [np.sum(abs(coef*scan.photocurrent - self.scans[reference].photocurrent)) for coef in coefs]
             scan.photocurrent *= coefs[np.argmin(diff)]
         
+        
 def plot_XANES(set_of_scans, format_of_figure):
     figure = plt.figure(1)
     if format_of_figure is 'big':
         for scan in set_of_scans.scans:
-            plt.plot(scan.energy,scan.photocurrent)
+            inf = np.argmin(abs(scan.energy - 1550))
+            plt.plot(scan.energy[inf:],scan.photocurrent[inf:])
+#            plt.tick_params(axis='y',)
+            plt.gca().get_yaxis().set_ticks([])
+            plt.xlim(1551,1600)
+            plt.ylim(-2E-4,25E-4)
+#            plt.savefig('.\\Figures\\' + set_of_scans.name + '_XANES.jpg')
     elif format_of_figure is 'small':
-        selection = {'Al':[0,2,3]}
-        for scan in set_of_scans.scans[selection[set_of_scans.name]]:
-            plt.plot(scan.energy,scan.photocurrent)
+#        selection = {'Al':(0,1,2,3,4,5)}
+        colors = plt.cm.jet(np.linspace(0,1,set_of_scans.length))
+        plt.gca().set_color_cycle(colors)
+        for scan in set_of_scans.scans:#[selection[set_of_scans.name]]:
+            inf,sup = np.argmin(abs(scan.energy - 1566)),np.argmin(abs(scan.energy - 1575))
+            plt.plot(scan.energy[inf:sup],scan.photocurrent[inf:sup])
+#            colors = plt.cm.jet(np.linspace(1,256,set_of_scans.length))
+#            plt.gca().set_color_cycle(colors)
+#            plt.gca().set_prop_cycle(cycler('color', colors))
+            plt.xlim(1566,1575)
+#            plt.gca().get_yaxis().set_ticks([])
+
     print 'Name, type, parameters of the scan'
     return figure
     
-##def plot_QE():
-##    
+def plot_QE(set_of_scans,format_of_figure):
+    figure = plt.figure(2)
+    if format_of_figure is 'big':
+        print 'BGG'
+    elif format_of_figure is 'small':
+        print 'Kirikou'
+        for scan in set_of_scans.scans:
+            plt.plot(scan.time,scan.QE)
+            plt.set_cmap('jet')
+
 ##def plot_correlation_QE_XANES_P():
 ##    
 ##def plot_correlation_CS_I():
@@ -74,8 +102,5 @@ def plot_XANES(set_of_scans, format_of_figure):
 ##
 ##def plot_fit():
 
-s = Scan('AlLirp','XANES',0)
-S = Set_of('Alrp','XANES')
+S = Set_of('Al','XANES')
 plot_XANES(S,'big')
-#plt.plot(s.time,s.QE)
-#plt.plot(s.energy,s.photocurrent)
