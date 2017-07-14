41c5a7c5b6f17b594f8385290dc0932b7b1acc1d
SmnThms <simon.thomas8@gmail.com>
2017-07-14 15:08:17 +0200

    Add files via upload

--- figure_generator.py
+++ figure_generator.py
@@ -8,6 +8,8 @@ from __future__ import division
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import colors as col
+from matplotlib import colorbar as clb
+from matplotlib import patches as patches
 
 
 class Scan:
@@ -55,7 +57,7 @@ class Set:
         for scan in self.scans:
             reference = ref_scan[self.name]
             coefs = np.arange(10000)*2/10000
-            limit1, limit2, limit3 = 1566, 1574, 1587
+            limit1, limit2, limit3 = 1567, 1574, 1599
             diff = [np.sum(abs(coef*scan.photocurrent[:scan.arg(limit1)] - self.scans[reference].photocurrent[:scan.arg(limit1)])) \
                     + np.sum(abs(coef*scan.photocurrent[scan.arg(limit2):scan.arg(limit3)] - self.scans[reference].photocurrent[scan.arg(limit2):scan.arg(limit3)])) \
                     for coef in coefs]
@@ -65,10 +67,24 @@ def cmap(nb_plots):
     colormap = plt.cm.jet
     plt.gca().set_color_cycle([colormap(i) for i in np.linspace(0, 0.9, nb_plots)])
 
-def plot_XANES(set_of_scans, format_of_figure):
-    figure = plt.figure(1)
+def plot_XANES(set_of_scans, format_of_figure, colours='jet'):
+    fig = plt.figure(1,figsize=(6,6))
     if format_of_figure is 'big':
-        cmap(len(set_of_scans.scans))
+        if colours is 'b&w':
+            colormap = plt.cm.Greys
+            plt.gca().set_color_cycle([colormap(0.9-i) for i in np.linspace(0, 0.7, set_of_scans.length)])
+        if colours is 'jet':
+            colormap = plt.cm.jet
+            plt.gca().set_color_cycle([colormap(i) for i in np.linspace(0, 0.9, set_of_scans.length)])
+        sm = plt.cm.ScalarMappable(cmap=colormap, norm=plt.Normalize(vmin=0, vmax=1))
+        sm._A = []
+#        mymap = col.LinearSegmentedColormap.from_list('mycolors',['blue','red'])
+#        cmap(len(set_of_scans.scans))
+#        Z = [[0,0],[0,0]]
+#        cbmin,cbmax,cbsteps = 1,set_of_scans.length,
+#        levels = range(set_of_scans.length)
+#        CS3 = plt.contourf(Z, levels, cmap=mymap)
+#        plt.clf()
         for scan in set_of_scans.scans:
             inf = np.argmin(abs(scan.energy - 1550))
             plt.plot(scan.energy[inf:],scan.photocurrent[inf:])
@@ -76,8 +92,18 @@ def plot_XANES(set_of_scans, format_of_figure):
             plt.gca().get_yaxis().set_ticks([])
             plt.xlim(1551,1600)
 #            plt.ylim(-2E-4,25E-4)
-            plt.xlabel('Energy (eV)')
-            plt.ylabel('Photocurrent (arb. units)')
+        plt.xlabel('Energy (eV)')
+        plt.ylabel('Photocurrent (arb. units)')
+        plt.gca().add_patch(patches.Rectangle((0.6,0.1),0.3,0.03,fill=False))
+        plt.show()
+        ax_cb = fig.add_axes([0.63,0.2,0.2,0.02])
+        cb= clb.Colorbar(ax_cb,sm,orientation='horizontal')
+#        cb = plt.colorbar(sm,cax=ax_cb,orientation='horizontal',pad=0.2)
+        cb.set_ticks(np.arange(set_of_scans.length)/(set_of_scans.length-1))
+        cb.set_ticklabels(np.arange(set_of_scans.length)+1)
+#        cb.set_ticks()
+        ax_cb.text(0,1.6,'Scan number')
+#        cb.set_label('Scan number')
 #            plt.savefig('.\\Figures\\' + set_of_scans.name + '_XANES.jpg')
     elif format_of_figure is 'small':
 #        selection = {'Al':(0,1,2,3,4,5)}
@@ -93,12 +119,39 @@ def plot_XANES(set_of_scans, format_of_figure):
 #            plt.gca().get_yaxis().set_ticks([])
 
     print 'Name, type, parameters of the scan'
-    return figure
+    return fig
     
-def plot_QE(set_of_scans,format_of_figure):
-    figure = plt.figure(2)
+def plot_QE(set_of_scans,format_of_figure,colours='bw'):
+    fig = plt.figure(2,figsize=(6,6))
     if format_of_figure is 'big':
-        print 'BGG'
+        if colours is 'bw':
+            colormap = plt.cm.Greys
+            plt.gca().set_color_cycle([colormap(0.9-i) for i in np.linspace(0, 0.7, set_of_scans.length)])
+        if colours is 'jet':
+            colormap = plt.cm.jet
+            plt.gca().set_color_cycle([colormap(i) for i in np.linspace(0, 0.9, set_of_scans.length)])
+        sm = plt.cm.ScalarMappable(cmap=colormap, norm=plt.Normalize(vmin=0, vmax=1))
+        sm._A = []
+        for scan in set_of_scans.scans:
+            inf = 3
+#            inf = np.argmin(abs(scan.energy - 1550))
+            plt.plot(scan.time[inf:],scan.QE[inf:],'.')
+#            plt.tick_params(axis='y',)
+            plt.gca().get_yaxis().set_ticks([])
+#            plt.xlim(1551,1600)
+#            plt.ylim(-2E-4,25E-4)
+        plt.xlabel('Time (s)')
+        plt.ylabel('QE (arb. units)')
+        plt.gca().add_patch(patches.Rectangle((0.6,0.1),0.3,0.03,fill=False))
+        plt.show()
+        ax_cb = fig.add_axes([0.63,0.78,0.2,0.02])
+        cb= clb.Colorbar(ax_cb,sm,orientation='horizontal')
+#        cb = plt.colorbar(sm,cax=ax_cb,orientation='horizontal',pad=0.2)
+        cb.set_ticks(np.arange(set_of_scans.length)/(set_of_scans.length-1))
+        cb.set_ticklabels(np.arange(set_of_scans.length)+1)
+#        cb.set_ticks()
+        ax_cb.text(0,1.6,'Scan number')
+        
     elif format_of_figure is 'small':
         print 'Kirikou'
         for scan in set_of_scans.scans:
@@ -107,7 +160,7 @@ def plot_QE(set_of_scans,format_of_figure):
             
 nb = {'Al':6,'Alrp':9}
 ref_scan = {'Al':4,'Alrp':4}
-nonscaled_zone = {'Al':(0,)}
+#nonscaled_zone = {'Al':(0,)}
 
 
 ##def plot_correlation_QE_XANES_P():
@@ -119,5 +172,8 @@ nonscaled_zone = {'Al':(0,)}
 ##def plot_fit():
 
 plt.close('all')
-S = Set('Al','XANES')
-plot_XANES(S,'big')
+#S = Set('Al','XANES')
+#plot_XANES(S,'big')
+
+S = Set('Alrp','QE')
+plot_QE(S,'big')
